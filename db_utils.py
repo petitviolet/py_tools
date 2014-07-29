f63ca90ea351d78ab7339157c6b620c461c5c45c
petitviolet <violethero0820@gmail.com>
2014-07-29 23:30:42 +0900

    2と3を共存

--- db_utils.py
+++ db_utils.py
@@ -1,6 +1,4 @@
 # -*- encoding:utf-8 -*-
-'''
-'''
 import MySQLdb
 db_info = {
     'HOST': 'HOST',
--- fetch_html.py
+++ fetch_html.py
@@ -1,8 +1,5 @@
 # -*- encoding:utf-8 -*-
-'''
-
-'''
-
+from __future__ import print_function
 import requests
 
 
